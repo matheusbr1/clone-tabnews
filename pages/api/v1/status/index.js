aa0b0bd5d3ba9761c4770e772ff539689b404e67
Matheus <matheusbaron10@gmail.com>
2026-05-22 11:08:12 -0300

    feat: implement status endpoint to return database health metrics

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js"
 
-async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum")
-  console.log(result.rows[0])
-  res.status(200).json({ status: "healthy" })
+async function status(_, res) {
+  const updatedAt = new Date().toISOString()
+  const version = await database.query("SHOW server_version;")
+  const maxConnections = await database.query("SHOW max_connections;")
+  const databaseMaxConnections = maxConnections.rows[0].max_connections
+  const connectionsInUse = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: ["postgres"],
+  })
+  const databaseConectionsInUse = connectionsInUse.rows[0].count
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: version.rows[0].server_version,
+      max_connections: parseInt(databaseMaxConnections),
+      connections_in_use: databaseConectionsInUse,
+    },
+  })
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,36 @@
-test("GET to api/v1/status should return 200", async () => {
+async function fetchStatus() {
   const res = await fetch("http://localhost:3000/api/v1/status")
-  expect(res.status).toBe(200)
+  const body = await res.json()
+  return { status: res.status, body }
+}
+
+describe("GET /api/v1/status", () => {
+  test("should return 200", async () => {
+    const res = await fetchStatus()
+    expect(res.status).toBe(200)
+  })
+  test("should contain a valid updated_at timestamp", async () => {
+    const res = await fetchStatus()
+    expect(res.body.updated_at).toBeDefined()
+    const parsedUpdatedDate = new Date(res.body.updated_at).toISOString()
+    expect(res.body.updated_at).toBe(parsedUpdatedDate)
+  })
+  test("should contain the postgres version", async () => {
+    const res = await fetchStatus()
+    expect(res.body.database.version).toBeDefined()
+    expect(res.body.database.version).toBe("16.0")
+  })
+  test("should contain the postgres max_connections", async () => {
+    const res = await fetchStatus()
+    expect(res.body.database.max_connections).toEqual(100)
+    expect(typeof res.body.database.max_connections).toBe("number")
+    expect(res.body.database.max_connections).toBeDefined()
+  })
+  test("should contain the postgres connections_in_use", async () => {
+    const res = await fetchStatus()
+    console.log(res.body.database)
+    expect(typeof res.body.database.max_connections).toBe("number")
+    expect(res.body.database.connections_in_use).toEqual(1)
+    expect(res.body.database.connections_in_use).toBeDefined()
+  })
 })
